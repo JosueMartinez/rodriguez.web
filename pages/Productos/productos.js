5cf94b3d170917ffed2a4937a6d800b3b054676e
Josue Martinez <josuemartinez@Josues-MacBook-Pro.local>
2017-03-06 17:48:24 -0400

    agregando custom service para categorias y productos

--- pages/Productos/productos.js
+++ pages/Productos/productos.js
@@ -1,4 +1,65 @@
-(function(){
+(function () {
     var prod = angular.module('producto', []);
+   
+    prod.service('productoServ', ['$http', function ($http) {
+        
+            this.obtenerCategorias = function () {
+                return $http.get("http://rodriguez.api/api/categorias")
+                    .then(function (response) {
+                        return response.data;
+                    });
+            };
 
+            this.obtenerProductos = function () {
+                return $http.get("http://rodriguez.api/api/productos")
+                    .then(function (response) {
+                        productos = response.data;
+                        return productos;
+                    });
+                //fin productos
+            };
+
+            this.print = function () {
+                return $http.get("http://rodriguez.api/api/values")
+                    .then(function (response) {
+                        return response.data;
+                    });
+                
+            };
+    }]);
+
+    prod.controller('productoController', function ($scope, $uibModal, $log, $document, $http, productoServ) {
+        $scope.pagina = "Productos & Categorias";
+        $scope.sitio = "este es el sitio";
+        
+        productoServ.obtenerCategorias().then(function(data){
+            $scope.categorias = data;
+        });
+
+        productoServ.obtenerProductos().then(function(data){
+            $scope.productos = data;
+        });
+
+        //modal para agregar productoController
+        $scope.modalProducto = function (page, size, producto) {
+
+            $scope.modal = $uibModal.open({
+                animation: true,
+                size: size,
+                templateUrl: page,
+                scope: $scope,
+                controller: 'productoModalController',
+                controllerAs: 'producto'
+            });
+        };
+    });
+
+    prod.controller('productoModalController', function ($uibModalInstance, $scope) {
+
+        this.crearProducto = function () {
+            //TODO validation y crearProducto
+            console.log('crear producto');
+            alert('test');
+        };
+    });
 })();
--- rodriguezApp.js
+++ rodriguezApp.js
@@ -1,5 +1,5 @@
-(function(){
-  var app = angular.module('rodriguezApp', ['directives', 'routing', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', 'angularUtils.directives.dirPagination']);
+(function () {
+    var app = angular.module('rodriguezApp', ['producto','directives', 'routing', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', 'angularUtils.directives.dirPagination']);
 
     app.config(['$locationProvider', function ($locationProvider) {
         $locationProvider.hashPrefix('');
@@ -10,172 +10,17 @@
         $scope.sitio = "Control Panel";
     });
 
-    app.controller('bonoController', function ($scope, $routeParams){
-
-        if ($routeParams.id){
+    app.controller('bonoController', function ($scope, $routeParams) {
+        if ($routeParams.id) {
             $scope.pagina = "Bono";
             $scope.sitio = "Detalles";
+
             //TODO a consultar con servicio
-            $scope.bono = {'id': '134','remitente': 'Juan Perez','destinatario': 'Hector Gomez','fecha': '2016-03-21','monto': 4500.00,'estado': 'Emitido'}
-        }else{
+            $scope.bono = { 'id': '134', 'remitente': 'Juan Perez', 'destinatario': 'Hector Gomez', 'fecha': '2016-03-21', 'monto': 4500.00, 'estado': 'Emitido' }
+        } else {
             $scope.pagina = "Bonos";
             $scope.sitio = "Listado";
-            $scope.bonosList = bonos;
+            $scope.bonosList = [];
         }
     });
-
-    app.controller('productoController', function ($scope, $uibModal, $log, $document, $http) {
-        $scope.pagina = "Productos & Categorias";
-        $scope.sitio = "";
-        
-        //obteniendo categorias
-        $http.get("http://rodriguez.api/api/categorias")
-        .then(function(response) {
-          $scope.categorias = response.data;
-          console.log('categorias: ' + response.data);
-        })
-        .catch(function(response) {
-          console.error('Error cargando categorias', response.status, response.data);
-        });
-        //fin categorias
-
-        //obteniendo productos
-        // $scope.productos = productos;
-        $http.get("http://rodriguez.api/api/productos")
-        .then(function(response) {
-          $scope.productos = response.data;
-          console.log('productos: ' + response.data);
-        })
-        .catch(function(response) {
-          console.error('Error cargando productos', response.status, response.data);
-        });
-        //fin productos
-    });
-
-    app.controller('ModalDemoCtrl', function ($uibModal, $log, $document) {
-      var $ctrl = this;
-      $ctrl.items = ['item1', 'item2', 'item3'];
-
-      $ctrl.animationsEnabled = true;
-
-      $ctrl.open = function (size, parentSelector) {
-        var parentElem = parentSelector ? 
-          angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
-        var modalInstance = $uibModal.open({
-          animation: $ctrl.animationsEnabled,
-          ariaLabelledBy: 'modal-title',
-          ariaDescribedBy: 'modal-body',
-          templateUrl: '/pages/Productos/agregarProducto.html',
-          controller: 'ModalInstanceCtrl',
-          controllerAs: '$ctrl',
-          size: size,
-          appendTo: parentElem,
-          resolve: {
-            items: function () {
-              return $ctrl.items;
-            }
-          }
-        });
-
-        modalInstance.result.then(function (selectedItem) {
-          $ctrl.selected = selectedItem;
-        }, function () {
-          $log.info('Modal dismissed at: ' + new Date());
-        });
-      };
-
-      $ctrl.openComponentModal = function () {
-        var modalInstance = $uibModal.open({
-          animation: $ctrl.animationsEnabled,
-          component: 'modalComponent',
-          resolve: {
-            items: function () {
-              return $ctrl.items;
-            }
-          }
-        });
-
-        modalInstance.result.then(function (selectedItem) {
-          $ctrl.selected = selectedItem;
-        }, function () {
-          $log.info('modal-component dismissed at: ' + new Date());
-        });
-      };
-
-      $ctrl.openMultipleModals = function () {
-        $uibModal.open({
-          animation: $ctrl.animationsEnabled,
-          ariaLabelledBy: 'modal-title-bottom',
-          ariaDescribedBy: 'modal-body-bottom',
-          templateUrl: 'stackedModal.html',
-          size: 'sm',
-          controller: function($scope) {
-            $scope.name = 'bottom';  
-          }
-        });
-
-        $uibModal.open({
-          animation: $ctrl.animationsEnabled,
-          ariaLabelledBy: 'modal-title-top',
-          ariaDescribedBy: 'modal-body-top',
-          templateUrl: 'stackedModal.html',
-          size: 'sm',
-          controller: function($scope) {
-            $scope.name = 'top';  
-          }
-        });
-      };
-
-      $ctrl.toggleAnimation = function () {
-        $ctrl.animationsEnabled = !$ctrl.animationsEnabled;
-      };
-    });
-
-    app.controller('ModalInstanceCtrl', function ($uibModalInstance, items) {
-      var $ctrl = this;
-      $ctrl.items = items;
-      $ctrl.selected = {
-        item: $ctrl.items[0]
-      };
-
-      $ctrl.ok = function () {
-        $uibModalInstance.close($ctrl.selected.item);
-      };
-
-      $ctrl.cancel = function () {
-        $uibModalInstance.dismiss('cancel');
-      };
-    });
-
-    var bonos = [{
-
-            'id': '134',
-            'remitente': 'Juan Perez',
-            'destinatario': 'Hector Gomez',
-            'fecha': '2016-03-21',
-            'monto': 4500.00,
-            'estado': 'Emitido'
-        },
-        {
-            'id': '134',
-            'remitente': 'Juan Perez',
-            'destinatario': 'Hector Gomez',
-            'fecha': '2016-03-21',
-            'monto': 4500.00,
-            'estado': 'Emitido'
-        },
-        {
-            'id': '134',
-            'remitente': 'Juan Perez',
-            'destinatario': 'Hector Gomez',
-            'fecha': '2016-03-21',
-            'monto': 4500.00,
-            'estado': 'Emitido'
-        }];
-
-
-
-
-
-
 })();
