def72d8bd953f1eed807b76e79e5982b915b6407
josereyes283 <jreyes283@gmail.com>
2019-02-19 18:30:08 -0400

    cambio propiedades

--- pages/Bonos/bonos.js
+++ pages/Bonos/bonos.js
@@ -101,8 +101,8 @@
                     $scope.bonos = data;
                     $scope.bonos.forEach(function (element) {
                         // element.cliente.nombreCompleto = element.cliente.nombres + ' ' + element.cliente.apellidos;
-                        element.nombreDestinoCompleto = element.nombreDestino + ' ' + element.apellidoDestino;
-                        element.montoRd = element.monto * element.tasa.valor;
+                        element.NombreDestinoCompleto = element.NombreDestino + ' ' + element.ApellidoDestino;
+                        element.MontoRd = element.Monto * element.Tasa.Valor;
                     }, this);
 
                 });
